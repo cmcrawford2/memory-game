5c75ff56b2cd71996a7019ec583be916930a8581
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2023-10-25 11:16:03 -0500

    formatted time to display minutes and seconds

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,12 +1,33 @@
 import PropTypes from "prop-types";
 
 export default function Modal(props) {
+  const minutes = Math.floor(props.nTotalTime / 60);
+  const seconds = props.nTotalTime - (minutes * 60);
+  var totalTimeDisplay = "";
+  switch(minutes) {
+    case 0:
+      break;
+    case 1:
+      totalTimeDisplay = "1 minute & ";
+      break;
+      default:
+        totalTimeDisplay = minutes + " minutes & ";
+  }
+
+  switch(seconds) {
+    case 1:
+      totalTimeDisplay += seconds + " second";
+      break;
+    default:
+      totalTimeDisplay += seconds + " seconds";
+  }
+  
   return (
     <div className="modal hidden" data-id="modal">
       <div className="modal-contents">
         <h1>game over!</h1>
         <h2>{`you took ${props.nTries} tries`}</h2>
-        <h2>{`(${props.nTotalTime} seconds)`}</h2>
+        <h2>{`(${totalTimeDisplay})`}</h2>
         <button onClick={props.restart}>
           <h3>play again</h3>
         </button>
